e1f24c2a929819198eec981c587c68755856233d
RegaloOmar <regaladomendozaomar@gmail.com>
2022-05-14 01:21:13 -0500

    Initial Commit

---
+++ Star Wars Legacy/Star_Wars_LegacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_Wars_LegacyApp.swift
+//  Star Wars Legacy
+//
+//  Created by Omar Regalado on 14/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Star_Wars_LegacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
